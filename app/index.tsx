fb780035c3204b8a87db484f099a6fcdfb300381
Meesam <meesam.engineer@gmail.com>
2024-08-30 10:54:23 +0530

    Initial setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className=" flex-1 justify-center items-center bg-gray-800">
+      <Text className=" text-lg text-red-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
